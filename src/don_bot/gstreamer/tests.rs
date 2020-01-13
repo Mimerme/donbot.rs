100ad4ba49bd02f5e7eb9c9b0de7688c24285670
Mimerme <androssdragonq.9@gmail.com>
2020-01-13 02:47:55 -0500

    a

--- src/don_bot/gstreamer/tests.rs
+++ src/don_bot/gstreamer/tests.rs
@@ -7,12 +7,12 @@ fn test_generate_overlay(){}
 fn test_concat_clips(){
     use super::*;
 
-    let cfg = Ini::load_from_file("config.ini").unwrap();
-    let pipeline = stitch_videos_pipeline(vec!["/home/mimerme/projects/donbot.rs/downloads/AT-cm585662124.mp4".to_string(),
+    //let cfg = Ini::load_from_file("config.ini").unwrap();
+    //let pipeline = stitch_videos_pipeline(vec!["/home/mimerme/projects/donbot.rs/downloads/AT-cm585662124.mp4".to_string(),
                 "/home/mimerme/Downloads/SampleVideo_1280x720_1mb.mp4".to_string()], &cfg).unwrap();
    
     //NOTE: many errors won't arise in the pipeline until we run it
-    run_pipeline(pipeline);
+    //run_pipeline(pipeline);
 
 }
 
